726408b48e8ecf3bf1a78d0e420b988bd88b4449
therealzonder <zanon.mark@gmail.com>
2023-01-19 21:36:58 -0500

    Initial commit

--- src/main/java/com/toxicnpcs/ToxicNpcsConfig.java
+++ src/main/java/com/toxicnpcs/ToxicNpcsConfig.java
@@ -4,16 +4,8 @@ import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
-@ConfigGroup("example")
+@ConfigGroup("ToxicNpcs")
 public interface ToxicNpcsConfig extends Config {
-	@ConfigItem(
-			keyName = "greeting",
-			name = "Welcome Greeting",
-			description = "The message to show to the user when they login"
-	)
-	default String greeting() {
-		return "Hello";
-	}
 
 	@ConfigItem(
 			keyName = "toxicNPCs",
